28a9ee4ecadf6525981301f3bb12359f94e2e050
charliephelpscodes <164572692+charliephelpscodes@users.noreply.github.com.>
2024-05-27 18:53:08 -0700

    Write calculation functions and test in console
    Write Add, Subtract, Multiply, and Divide functions. Check to make sure
    they work properly by logging results to console.

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //Calculation Functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+let a = 10;
+let b = 5;
+
+console.log(add(a,b), subtract(a,b), multiply(a,b), divide(a,b));
+
+
+
+
 
