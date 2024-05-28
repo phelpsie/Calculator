ea81c20125243448a7fdcfab3f77e11a87e0d13c
charliephelpscodes <164572692+charliephelpscodes@users.noreply.github.com.>
2024-05-28 13:53:13 -0700

    Add query selectors for all buttons and display

--- script.js
+++ script.js
@@ -38,6 +38,41 @@ function operate(operator, firstOperand, secondOperand) {
 
 console.log(operate('-', 10, 20));
 
+//Event Listeners
+//Query Selectors
+
+let display = document.querySelector('.display');
+
+//First Row:...
+let clearBtn = document.querySelector('.clear');
+let plusminusBtn = document.querySelector('.plus-minus');
+let percentBtn = document.querySelector('.percent');
+let divideBtn = document.querySelector('.divide');
+
+//Second Row:...
+let sevenBtn = document.querySelector('.seven');
+let eightBtn = document.querySelector('.eight');
+let nineBtn = document.querySelector('.nine');
+let multiplyBtn = document.querySelector('.multiply');
+
+//Third Row:...
+let fourBtn = document.querySelector('.four');
+let fiveBtn = document.querySelector('.five');
+let sixBtn = document.querySelector('.six');
+let minusBtn = document.querySelector('.minus');
+
+//Fourth Row:...
+let oneBtn = document.querySelector('.one');
+let twoBtn = document.querySelector('.two');
+let threeBtn = document.querySelector('.three');
+let addBtn = document.querySelector('.add');
+
+//Fifth Row:...
+let zeroBtn = document.querySelector('.zero');
+let decimalBtn = document.querySelector('.decimal');
+let equalsBtn = document.querySelector('.equals');
+
+
 
 
 
