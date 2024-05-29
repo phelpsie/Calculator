2d276566611db375571c42da476db974b36c5f9c
charliephelpscodes <164572692+charliephelpscodes@users.noreply.github.com.>
2024-05-28 18:58:06 -0700

    Add event listeners to each operand and operator
    Log these values to the display, as well as to a displayValue variable.
    Console.log is to show it works.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+let displayValue;
+
+
 //Calculator Equation Variables
 let firstOperand;
 let operator;
 let secondOperand;
+
 
 //Calculation Functions
 function add(a, b) {
@@ -38,39 +42,123 @@ function operate(operator, firstOperand, secondOperand) {
 
 console.log(operate('-', 10, 20));
 
-//Event Listeners
+
 //Query Selectors
 
 let display = document.querySelector('.display');
 
-//First Row:...
+
 let clearBtn = document.querySelector('.clear');
 let plusminusBtn = document.querySelector('.plus-minus');
 let percentBtn = document.querySelector('.percent');
 let divideBtn = document.querySelector('.divide');
 
-//Second Row:...
+
 let sevenBtn = document.querySelector('.seven');
 let eightBtn = document.querySelector('.eight');
 let nineBtn = document.querySelector('.nine');
 let multiplyBtn = document.querySelector('.multiply');
 
-//Third Row:...
+
 let fourBtn = document.querySelector('.four');
 let fiveBtn = document.querySelector('.five');
 let sixBtn = document.querySelector('.six');
-let minusBtn = document.querySelector('.minus');
+let subtractBtn = document.querySelector('.subtract');
 
-//Fourth Row:...
+
 let oneBtn = document.querySelector('.one');
 let twoBtn = document.querySelector('.two');
 let threeBtn = document.querySelector('.three');
 let addBtn = document.querySelector('.add');
 
-//Fifth Row:...
+
 let zeroBtn = document.querySelector('.zero');
 let decimalBtn = document.querySelector('.decimal');
 let equalsBtn = document.querySelector('.equals');
+
+
+
+//Event Listeners
+
+clearBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "";
+    console.log(displayValue);
+})
+
+//...
+sevenBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "7";
+    console.log(displayValue);
+})
+
+eightBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "8";
+    console.log(displayValue);
+})
+
+nineBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "9";
+    console.log(displayValue);
+})
+
+multiplyBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "x";
+    console.log(displayValue);
+})
+
+//...
+fourBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "4";
+    console.log(displayValue);
+})
+
+fiveBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "5";
+    console.log(displayValue);
+})
+
+sixBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "6";
+    console.log(displayValue);
+})
+
+subtractBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "-";
+    console.log(displayValue);
+})
+
+//...
+oneBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "1";
+    console.log(displayValue);
+})
+
+twoBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "2";
+    console.log(displayValue);
+})
+
+threeBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "3";
+    console.log(displayValue);
+})
+
+addBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "+";
+    console.log(displayValue);
+})
+
+//...
+zeroBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = "0";
+    console.log(displayValue);
+})
+
+decimalBtn.addEventListener('click', function() {
+    let displayValue = display.textContent = ".";
+    console.log(displayValue);
+})
+
 
 
 
